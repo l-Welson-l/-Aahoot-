108b60afbc9a6ed7e8a8b4541c077771bd431016
Welson <kcwelson14@gmail.com>
2024-11-22 20:45:23 +0000

    Added Logout View

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import UserLogin, home, RegisterPage
+from .views import UserLogin, home, RegisterPage, UserLogout
 urlpatterns = [
     path('', home.as_view(), name="home" ),
     path('login/',UserLogin.as_view(), name="login" ),
     path('register/',RegisterPage.as_view(), name="register"),
-    
+    path('logout/', UserLogout.as_view(), name="logout")
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import FormView, UpdateView, CreateView, DeleteView
+from django.views.generic import View 
 from django.urls import reverse_lazy
 from .models import Quiz, Answer, CorrectAnswer, UserAnswer
 
@@ -26,5 +27,7 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super().form_valid(form)
 
-    
-
+class UserLogout(View):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('home')
