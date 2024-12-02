f4fe04b32fc2d1eeee45374a3d4a20b0dfecc8b1
Welson <kcwelson14@gmail.com>
2024-12-02 18:50:03 +0000

    added form

---
+++ home/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Quiz, Question, Answer
+
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        fields = ['title', 'description']
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['text']
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['text', 'is_correct']
